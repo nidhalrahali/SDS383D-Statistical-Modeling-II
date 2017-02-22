fc27f0fb2b78a0490b1512078f47ff3e017c4d00
yinanzhu12 <welltempered.ivan@gmail.com>
2017-02-22 13:30:41 -0600

    update

---
+++ exercise3/predictionerror.R
@@ -0,0 +1,16 @@
+source('~/GitHub/SDS383D-course-work/exercise3/weight.R')
+
+predictionerror=function(ytrain,xtrain,ytest,xtest,h,t){
+  #normalize all data
+  ymean=mean(ytrain)
+  ynorm=ytrain-matrix(ymean,ncol=length(ytrain))
+  xnorm=xtrain-matrix(mean(xtrain),ncol=length(xtrain))
+  xtestnorm=xtest-matrix(mean(xtrain),ncol=length(xtest))
+  
+  # fit smoother and record the error
+  error=matrix(ncol=length(ytest))
+  for(i in 1:length(ytest)){
+    error[i]=ynorm %*% t(weight(xtestnorm[i],xnorm,h,type=t))+ymean-ytest[i]
+  }
+  return(error)
+}
--- exercise3/testmodel.R
+++ exercise3/testmodel.R
@@ -1,16 +1,56 @@
-source('~/GitHub/SDS383D-course-work/exercise3/weight.R')
+source('~/GitHub/SDS383D-course-work/exercise3/predictionerror.R')
 
-testmodel=function(ytrain,xtrain,ytest,xtest,h,t){
-  #normalize all data
-  ymean=mean(ytrain)
-  ynorm=ytrain-matrix(ymean,ncol=length(ytrain))
-  xnorm=xtrain-matrix(mean(xtrain),ncol=length(xtrain))
-  xtestnorm=xtest-matrix(mean(xtrain),ncol=length(xtest))
-  
-  # fit smoother and record the error
-  error=matrix(ncol=length(ytrain))
-  for(i in 1:length(ytrain)){
-    error[i]=ynorm %*% t(weight(xtestnorm[i],xnorm,h,type=t))+ymean-ytest[i]
-  }
-  return(error)
+# smooth
+f=function(x){
+  return=x^3/10-x
 }
+plot(f)
+
+# wiggly
+g=function(x){
+  return=sin(x*100)+x
+}
+plot(g)
+
+# number of samples
+n=100
+
+x=matrix(ncol=n)
+for(i in 1:n){
+  x[i]=-5+10/n*i
+}
+
+# seperate into training and test
+xrand=sample(x,n)
+xtrain=xrand[1:(n*2/3)]
+xtest=xrand[(n*2/3+1):n]
+
+# smooth and quiet
+ytrain=f(xtrain)+rnorm(length(xtrain),sd=0.5)
+ytest=f(xtest)+rnorm(length(xtest),sd=0.5)
+
+# smooth and noisy
+ytrain=f(xtrain)+rnorm(length(xtrain),sd=2)
+ytest=f(xtest)+rnorm(length(xtest),sd=2)
+
+# wiggly and quiet
+ytrain=g(xtrain)+rnorm(length(xtrain),sd=0.5)
+ytest=g(xtest)+rnorm(length(xtest),sd=0.5)
+
+# wiggly and noisy
+ytrain=g(xtrain)+rnorm(length(xtrain),sd=2)
+ytest=g(xtest)+rnorm(length(xtest),sd=2)
+
+
+plot(ytrain~xtrain)
+
+h=matrix(ncol=100)
+for(i in 1:100){
+  h[i]=i/10+2
+}
+sqerror=matrix(ncol=100)
+for(i in 1:100){
+error=predictionerror(ytrain,xtrain,ytest,xtest,h[i],"gaussian")
+sqerror[i]=tcrossprod(error,y=NULL)/ncol(error)
+}
+plot(sqerror~h)
