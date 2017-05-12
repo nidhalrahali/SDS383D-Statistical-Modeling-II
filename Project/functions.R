0fa8ae130cb7bb0e73636117bef4aa9cd7bddedf
yinanzhu12 <welltempered.ivan@gmail.com>
2017-05-11 21:38:30 -0500

    u

--- Project/functions.R
+++ Project/functions.R
@@ -1,49 +1,66 @@
 library(MCMCpack)
 library(truncnorm)
 
-p=function(y,h,sigma2,mu){
-  return=h^(-1.5)*exp(-y^2/(2*h)-(log(h)-mu)^2/(2*sigma2))
+ln_p=function(y,h,sigma2,mu){
+  return=-1.5*log(h)-y^2/(2*h)-(log(h)-mu)^2/(2*sigma2)
 }
 
-nexth=function(y,h,sigma2,mu){
+updateh=function(y,h,lhleft,lhright,alpha,delta,sigma_nu2){
+  sigma2=sigma_nu2/(1+delta^2)
+  mu=(delta*(lhleft+lhright)+(1-delta)*alpha)/(1+delta^2)
   s=(1-2*exp(sigma2))/(1-exp(sigma2))+0.5
   r=(s-1)*exp(mu+sigma2/2)+y^2/2
   qmode=r/(1+s)
-  c=1.1*p(y,qmode,sigma2,mu)/dinvgamma(qmode,scale=r,shape=s)
+  ln_c=log(1.2)+ln_p(y,qmode,sigma2,mu)-log(dinvgamma(qmode,scale=r,shape=s))
   hn=rinvgamma(1,shape=s,scale=r)
-  accept=p(y,hn,sigma2,mu)/(c*dinvgamma(hn,scale=r,shape=s))
+  accept=exp(ln_p(y,hn,sigma2,mu)-ln_c-log(dinvgamma(hn,scale=r,shape=s)))
   roll=runif(1)
-  if(roll>accept)hn=h
-  return=hn
+  rej=0
+  if(roll>accept){
+    hn=h
+    rej=1
+  }
+  return=list(newh=hn,reject=rej)
 }
 
-computemu=function(ln_h,delta,alpha){
-  n=length(ln_h)
-  mu=rep(0,n)
-  mu[1]=(ln_h[2]-alpha)/delta
-  mu[n]=delta*ln_h[n-1]+alpha
-  for(i in 2:(n-1))mu[i]=(delta*(ln_h[i+1]+ln_h[i-1])+(1-delta)*alpha)/(1+delta^2)
-  return=mu
+p_ratio=function(y,h1,h2,sigma2,mu){
+  return=(h2/h1)^1.5*exp(-y^2/(2*h1)+y^2/(2*h2)-((log(h1)-mu)^2-(log(h2)-mu)^2)/(2*sigma2))
 }
 
+updateh_rw=function(y,h,lhleft,lhright,alpha,delta,sigma_nu2,sd_rw){
+  sigma2=sigma_nu2/(1+delta^2)
+  mu=(delta*(lhleft+lhright)+(1-delta)*alpha)/(1+delta^2)
+  hn=rtruncnorm(1,a=0,mean=h,sd=sd_rw)
+  accept=p_ratio(y,hn,h,sigma2,mu)
+  roll=runif(1)
+  rej=0
+  if(roll>accept){
+    hn=h
+    rej=1
+  }
+  return=list(newh=hn,reject=rej)
+}
 
-
-sampler=function(y,nu_0,s_0,delta_0,sigma_delta2,alpha_0,sigma_alpha2,t,burnin){
+sampler=function(y,nu_0,s_0,delta_0,sigma_delta2,alpha_0,sigma_alpha2,t,burnin,sd_rw){
   n=length(y)
   delta=1
   alpha=0
-  h0=sd(y)^2
-  h=rep(h0,n)
+  #h0=sd(y)^2
+  #h=rep(h0,n)
+  h=rep(10,n)
+  ln_h=log(h)
   h_sample=matrix(nrow=n,ncol=t)
   alpha_sample=rep(0,t)
   delta_sample=rep(0,t)
   sigma_nu2_sample=rep(0,t)
+  upd=0
+  rej=0
   for(ite in 1:(t+burnin)){
     if(ite%%100==0)print(ite)
-    ln_h=log(h)
     s1=sum(ln_h)
     s2=sum(ln_h^2)
     s3=sum(ln_h[1:(n-1)]*ln_h[2:n])
+    
     sigma_nu_scale=(s_0+(n-1)*alpha^2+(1+delta^2)*s2-ln_h[1]^2-delta^2*ln_h[n]^2-2*alpha*((1-delta)*s1-ln_h[1]+delta*ln_h[n])-2*delta*s3)/2
     sigma_nu2=rinvgamma(1,shape=(nu_0+n-1)/2,scale=sigma_nu_scale)
     
@@ -54,19 +71,21 @@ sampler=function(y,nu_0,s_0,delta_0,sigma_delta2,alpha_0,sigma_alpha2,t,burnin){
     alphamean=(sigma_alpha2*((1-delta)*s1-ln_h[1]+delta*ln_h[n])+sigma_nu2*alpha_0)/(sigma_nu2+(n-1)*sigma_alpha2)
     alphasd=sqrt(sigma_nu2*sigma_alpha2/(sigma_nu2+(n-1)*sigma_alpha2))
     alpha=rnorm(1,mean=alphamean,sd=alphasd)
-    
-    mu=computemu(ln_h,delta,alpha)
-    newh=h
     #print(delta)
-    #newh[1]=nexth(y[1],h[1],sigma_nu2/delta^2,mu[1])
-    #newh[n]=nexth(y[n],h[n],sigma_nu2,mu[n])
-    sigma2=sigma_nu2/(1+delta^2)
-    for(i in 2:(n-1))newh[i]=nexth(y[i],h[i],sigma2,mu[i])
-    h=newh
-    eps=rnorm(1)
-    h[1]=exp(alpha+delta*log(h[2])+eps*sqrt(sigma_nu2))
-    eps=rnorm(1)
-    h[n]=exp(alpha+delta*log(h[n-1])+eps*sqrt(sigma_nu2))
+    
+    for(i in 2:(n-1)){
+        #r=updateh(y[i],h[i],ln_h[i-1],ln_h[i+1],delta,alpha,sigma_nu2)
+        r=updateh_rw(y[i],h[i],ln_h[i-1],ln_h[i+1],delta,alpha,sigma_nu2,sd_rw)
+        h[i]=r$newh
+        rej=rej+r$reject
+        upd=upd+1
+        ln_h[i]=log(h[i])
+    }
+    ln_h[1]=rnorm(1,mean=alpha+delta*ln_h[2],sd=sqrt(sigma_nu2))
+    h[1]=exp(ln_h[1])
+    ln_h[n]=rnorm(1,mean=alpha+delta*ln_h[n-1],sd=sqrt(sigma_nu2))
+    h[n]=exp(ln_h[n])
+    
     if(ite>burnin){
     alpha_sample[ite-burnin]=alpha
     delta_sample[ite-burnin]=delta
@@ -74,5 +93,5 @@ sampler=function(y,nu_0,s_0,delta_0,sigma_delta2,alpha_0,sigma_alpha2,t,burnin){
     h_sample[,ite-burnin]=h
   }
   }
-  return=list(h_sample=h_sample,alpha_sample=alpha_sample,delta_sample=delta_sample,sigma_nu2_sample=sigma_nu2_sample)
+  return=list(h_sample=h_sample,alpha_sample=alpha_sample,delta_sample=delta_sample,sigma_nu2_sample=sigma_nu2_sample,rejectionrate=rej/upd)
 }
--- Project/svol.R
+++ Project/svol.R
@@ -1,40 +1,46 @@
 library(stats)
 source('~/GitHub/SDS383D-course-work/Project/functions.R')
 
-y=-0.00034
-sigma2=0.0005
-mu=-2.6
+x=-106
+sigma2=0.001
+mu=1
 #y=0.1
 #sigma2=0.1
 #mu=1
 h=seq(from=1,to=10,by=0.01)
 s=(1-2*exp(sigma2))/(1-exp(sigma2))+0.5
-r=(s-1)*exp(mu+sigma2/2)+y^2/2
+r=(s-1)*exp(mu+sigma2/2)+x^2/2
 qmode=r/(1+s)
-c=p(y,qmode,sigma2,mu)/dinvgamma(qmode,scale=r,shape=s)
-plot(p(y,h,sigma2,mu)~h,type='l',col="black",ylab="")
+c=p(x,qmode,sigma2,mu)/dinvgamma(qmode,scale=r,shape=s)
+plot(p(x,h,sigma2,mu)~h,type='l',col="black",ylab="")
 lines(c*dinvgamma(h,shape=s,scale=r)~h,col="blue")
 lines(1.1*c*dinvgamma(h,shape=s,scale=r)~h,col="red")
 lines(1.2*c*dinvgamma(h,shape=s,scale=r)~h,col="green")
 legend(x=6.8,y=0.25,legend=c("p","c=1","c=1.1","c=1.2"),fill=c("black","blue","red","green"))
 
 
-sp <- read.csv("~/GitHub/SDS383D-course-work/Project/SP.csv")
-plot(sp$S.P.500,type="l")
-ar1=ar.ols(x=sp$S.P.500,order.max=1)
+sp <- read.csv("~/GitHub/SDS383D-course-work/Project/SP1980-1987.csv")
+plot(sp$Close,type="l")
+ar1=ar.ols(x=sp$Close,order.max=1)
 lines(ar1$ar[1,1,1]*(sp$S.P.500-ar1$x.mean)+ar1$x.mean,col="red")
 y=ar1$resid
 y=na.omit(y)
 plot(y,type='l')
 
-n=length(sp$S.P.500)
-y1=(sp$S.P.500[2:n]-sp$S.P.500[1:(n-1)])/sp$S.P.500[1:(n-1)]
-plot(y1,type='l')
-ar2=ar.ols(x=y1,order.max=1)
+n=length(sp$Close)
+logchange=log(sp$Close[2:n])-log(sp$Close[1:(n-1)])
+plot(logchange,type='l')
 
-sample=sampler(y1,1,1,0,10,0,10,2000,4000)
+y1=(sp$Close[2:n]-sp$Close[1:(n-1)])/sp$Close[1:(n-1)]
+plot(y1,type='l')
+
+sample=sampler(logchange,1,1,0,10,0,10,2000,1000,0.1)
 plot(sample$delta_sample,type='l',ylab="",main="delta")
+hist(sample$delta_sample,xlab='delta',ylab="",main="delta")
 plot(sample$alpha_sample,type='l',ylab="",main="alpha")
+hist(sample$alpha_sample,xlab='alpha',ylab="",main="alpha")
 plot(sample$sigma_nu2_sample,type='l',ylab="",main="sigma_nu2")
+hist(sample$sigma_nu2_sample,ylab="",main="sigma_nu2")
 plot(log(sample$h_sample[,1000]),type='l',ylab="log(h)",main="1000th iteration")
 plot(log(sample$h_sample[100,]),type='l',ylab="log(h)",main="evolution of h")
+sample$rejectionrate
